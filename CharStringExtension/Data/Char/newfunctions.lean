6a415ab6c5457b99a7ae1db43a052a5817c8db6a
Lolol-Yt <136053748+Lolol-Yt@users.noreply.github.com>
2024-06-11 12:36:53 -0700

    Update newfunctions.lean

--- CharStringExtension/Data/Char/newfunctions.lean
+++ CharStringExtension/Data/Char/newfunctions.lean
@@ -1,11 +1,12 @@
 def Char.boldify (char: Char) : Char :=
-if char.toNat>64 ∧ char.toNat<91 then Char.ofNat (char.toNat + 119743)
-else if char.toNat>96 ∧ char.toNat<123 then Char.ofNat (char.toNat + 119737)
-else if char.toNat>119859 ∧ char.toNat<119912 then Char.ofNat (char.toNat + 52)
+if char.toNat>64 ∧ char.toNat<91 then Char.ofNat (char.toNat + 119743) -- Uppercase
+else if char.toNat>96 ∧ char.toNat<123 then Char.ofNat (char.toNat + 119737) -- Lowercase
+else if char.toNat>47 ∧ char.toNat<58 then Char.ofNat (char.toNat + 120734) -- Numbers
+else if char.toNat>119859 ∧ char.toNat<119912 then Char.ofNat (char.toNat + 52) -- Italic
 else char
 
 def Char.italicize (char: Char) : Char :=
-if char.toNat>64 ∧ char.toNat<91 then Char.ofNat (char.toNat + 119795)
-else if char.toNat>96 ∧ char.toNat<123 then Char.ofNat (char.toNat + 119789)
-else if char.toNat>119807 ∧ char.toNat<119860 then Char.ofNat (char.toNat + 104)
+if char.toNat>64 ∧ char.toNat<91 then Char.ofNat (char.toNat + 119795) -- Uppercase
+else if char.toNat>96 ∧ char.toNat<123 then Char.ofNat (char.toNat + 119789) -- Lowercase
+else if char.toNat>119807 ∧ char.toNat<119860 then Char.ofNat (char.toNat + 104) -- Bold
 else char
